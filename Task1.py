912261bfee66cb14b45700f4a9f80bab9811d7ed
JTN111 <randhawa037@gmail.com>
2025-07-20 11:33:33 +0530

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -4,7 +4,7 @@ def fact(n):
     else:
      return n * (fact(n-1))
 #To take input on runtime, un-comment 1 and 2 and replace fact(5) with fact(n).
-n = input('Enter a number :')
+n = input('Enter a number: ')
 n= int(n)
 result= fact(n)
 print("factorial of",n, "is",result)
